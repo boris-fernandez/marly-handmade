090e3224c573f574726e15638547ebe5c4cecc43
boris-fernandez <164273833+boris-fernandez@users.noreply.github.com.>
2025-09-29 17:20:49 -0500

    chore(tabulacion):mejoro la tabulacion

--- marly-handmade-frontend/src/pages/Register.jsx
+++ marly-handmade-frontend/src/pages/Register.jsx
@@ -19,62 +19,101 @@ export default function Register() {
   return (
     <>
       <Header />
-      <div className="register-container">
-      <div className="register-box">
-        <h2 className="register-title">Register</h2>
+      <div className='register-container'>
+        <div className='register-box'>
+          <h2 className='register-title'>Register</h2>
 
-        <form className="register-form" onSubmit={handleSubmit}>
-          <div className="form-group">
-            <input id="username" type="text" placeholder="Username" required />
-          </div>
-
-          <div className="form-group">
-            <input id="correo" type="email" placeholder="Email" required />
-          </div>
-          
-          <div className="form-group">
-            <input id="password" type="password" placeholder="Password" required />
-          </div>
-
-          <div className="form-group">
-              <input id="confirmPassword" type="password" placeholder="Confirm Password" required />
+          <form className='register-form' onSubmit={handleSubmit}>
+            <div className='form-group'>
+              <input
+                id='username'
+                type='text'
+                placeholder='Username'
+                required
+              />
             </div>
 
-          <div className="form-group">
-            <input id="nombres" type="text" placeholder="First Name(s)" required />
-          </div>
+            <div className='form-group'>
+              <input id='correo' type='email' placeholder='Email' required />
+            </div>
 
-          <div className="form-group">
-            <input id="apellidos" type="text" placeholder="Last Name(s)" required />
-          </div>
+            <div className='form-group'>
+              <input
+                id='password'
+                type='password'
+                placeholder='Password'
+                required
+              />
+            </div>
 
-          <div className="form-group">
-            <input id="direccion" type="text" placeholder="Address" required />
-          </div>
+            <div className='form-group'>
+              <input
+                id='confirmPassword'
+                type='password'
+                placeholder='Confirm Password'
+                required
+              />
+            </div>
 
-          <div className="form-group">
-            <input id="fechaNacimiento" type="date" required />
-          </div>
+            <div className='form-group'>
+              <input
+                id='nombres'
+                type='text'
+                placeholder='First Name(s)'
+                required
+              />
+            </div>
 
-          <div className="form-group">
-            <input id="identificacion" type="text" placeholder="ID Number" required />
-          </div>
+            <div className='form-group'>
+              <input
+                id='apellidos'
+                type='text'
+                placeholder='Last Name(s)'
+                required
+              />
+            </div>
 
-          <div className="form-group">
-            <input id="telefono" type="tel" placeholder="Phone Number" required />
-          </div>
+            <div className='form-group'>
+              <input
+                id='direccion'
+                type='text'
+                placeholder='Address'
+                required
+              />
+            </div>
 
+            <div className='form-group'>
+              <input id='fechaNacimiento' type='date' required />
+            </div>
 
-          <div className="form-links">
-            <Link to="/login">Already have an account? Sign In</Link>
-          </div>
+            <div className='form-group'>
+              <input
+                id='identificacion'
+                type='text'
+                placeholder='ID Number'
+                required
+              />
+            </div>
 
-          <button type="submit" className="register-btn">
-            Create Account
-          </button>
-        </form>
+            <div className='form-group'>
+              <input
+                id='telefono'
+                type='tel'
+                placeholder='Phone Number'
+                required
+              />
+            </div>
+
+            <div className='form-links'>
+              <Link to='/login'>Already have an account? Sign In</Link>
+            </div>
+
+            <button type='submit' className='register-btn'>
+              Create Account
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
       <Footer />
     </>
   );
