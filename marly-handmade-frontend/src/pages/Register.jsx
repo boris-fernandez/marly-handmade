9cb64a8bbcf2ea8cab2059fc2b098613c6530db8
MarcoALM2004 <u23258586@utp.edu.pe>
2025-09-28 21:43:32 -0500

    Sincronizando archivos con developer

---
+++ marly-handmade-frontend/src/pages/Register.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Register.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Register() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <Header />
+      <div className='register-container'>
+        <div className='register-box'>
+          <h2 className='register-title'>Register</h2>
+
+          <form className='register-form' onSubmit={handleSubmit}>
+            <div className='form-group'>
+              <input id='name' type='text' placeholder='Name' required />
+            </div>
+
+            <div className='form-group'>
+              <input id='email' type='email' placeholder='Email' required />
+            </div>
+
+            <div className='form-group'>
+              <input
+                id='password'
+                type='password'
+                placeholder='Password'
+                required
+              />
+            </div>
+
+            <div className='form-links'>
+              <Link to='/login'>Already have an account? Sign In</Link>
+            </div>
+
+            <button type='submit' className='register-btn'>
+              Create Account
+            </button>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
