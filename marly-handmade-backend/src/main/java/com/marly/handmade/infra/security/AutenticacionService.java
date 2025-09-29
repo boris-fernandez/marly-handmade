ce739be0292086f07c21a94a16adda28a4b4bee7
kahizer780 <164273833+boris-fernandez@users.noreply.github.com.>
2025-09-29 00:40:16 -0500

    feat(auth):Se agrego endpoints para registrarse y iniciar sesion

---
+++ marly-handmade-backend/src/main/java/com/marly/handmade/infra/security/AutenticacionService.java
@@ -0,0 +1,21 @@
+package com.marly.handmade.infra.security;
+
+import com.marly.handmade.domain.usuario.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacionService implements UserDetailsService {
+    private final UsuarioRepository usuarioRepository;
+
+    public AutenticacionService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByUsername(username);
+    }
+}
