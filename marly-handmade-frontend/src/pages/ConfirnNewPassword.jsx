c3a6653cc1184b9623ae6965767f0e42797626e1
MarcoALM2004 <u23258586@utp.edu.pe>
2025-09-29 19:14:17 -0500

    fixed/Recuperacion de contraseña

---
+++ marly-handmade-frontend/src/pages/ConfirnNewPassword.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Login.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function RecoverPassword() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <Header />
+      <div className='login-container'>
+        <div className='login-box'>
+          <h2 className='login-title'>New Password</h2>
+
+          <form className='login-form' onSubmit={handleSubmit}>
+            <div className='form-group'>
+              <input
+                id='password'
+                type='password'
+                placeholder='New Password'
+                required
+              />
+            </div>
+
+            <div className='form-group'>
+              <input
+                id='password'
+                type='password'
+                placeholder='Confirm New Password'
+                required
+              />
+            </div>
+
+            <button type='submit' className='login-btn'>
+              UPDATE
+            </button>
+          </form>
+
+          <div className='login-links'>
+            <Link to='/register'>Create account</Link>
+            <Link to='/'>Return to Store</Link>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
