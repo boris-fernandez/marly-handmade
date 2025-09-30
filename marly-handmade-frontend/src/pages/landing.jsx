573c1fc1f1f4840e2b6a79a2a1cbbc022f0cf878
Maryen <maryenaguilarzuniga@gmail.com>
2025-09-30 02:26:01 -0500

    feat(landing): agrega componente Shop by Look

--- marly-handmade-frontend/src/pages/landing.jsx
+++ marly-handmade-frontend/src/pages/landing.jsx
@@ -4,6 +4,9 @@ import Hero from "../components/Hero";
 import ShortMarlyDescriptionLanding from "../components/ShortMarlyDescriptionLanding";
 import MostLoved from "../components/MostLoved";
 import MarlyCollections from "../components/MarlyCollections";
+import ShopByLook from "../components/ShopByLook";
+import MeetTheMaker from "../components/MeetTheMaker";
+import ShopInPerson from "../components/ShopInPerson";
 
 export default function CartPage() {
   return (
@@ -17,6 +20,12 @@ export default function CartPage() {
       <MostLoved />
 
       <MarlyCollections />
+
+      <ShopByLook />
+
+      <MeetTheMaker />
+
+      <ShopInPerson />
 
       <Footer />
     </>
