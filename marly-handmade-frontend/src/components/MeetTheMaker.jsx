afca59debb0745c90f5e149c4c045ebfdf84ff19
Josue049 <josue09guti@gmail.com>
2025-10-16 03:07:17 -0500

    fix(admin): actualización de estilos

--- marly-handmade-frontend/src/components/MeetTheMaker.jsx
+++ marly-handmade-frontend/src/components/MeetTheMaker.jsx
@@ -22,7 +22,7 @@ export default function MeetTheMaker() {
       >
         <iframe
           className="w-full h-full"
-          src={`https://www.youtube.com/embed/${videoId}?autoplay=1&controls=1`}
+          src={`https://www.youtube.com/embed/${videoId}?controls=1`}
           title="Meet the Maker"
           frameBorder="0"
           allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
