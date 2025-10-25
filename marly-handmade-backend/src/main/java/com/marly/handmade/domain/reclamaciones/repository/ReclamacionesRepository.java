c46693e3f02aed95f638c7b8d5cd22029c291cf9
xyrafi <U22240951@utp.edu.pe>
2025-10-25 02:46:55 -0500

    fix(libro-de-reclamaciones): Corrige mostrar por nombre

--- marly-handmade-backend/src/main/java/com/marly/handmade/domain/reclamaciones/repository/ReclamacionesRepository.java
+++ marly-handmade-backend/src/main/java/com/marly/handmade/domain/reclamaciones/repository/ReclamacionesRepository.java
@@ -4,8 +4,10 @@ import com.marly.handmade.domain.reclamaciones.modal.Reclamaciones;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReclamacionesRepository extends JpaRepository <Reclamaciones, Long> {
 
-    Reclamaciones findByCliente_Nombres(String nombre);
+    List<Reclamaciones> findByCliente_Nombres(String nombre);
 }
