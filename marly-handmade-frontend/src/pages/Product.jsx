c152ccf572859b8652cedf8b2e45939c09793187
MarcoALM2004 <u23258586@utp.edu.pe>
2025-10-28 18:41:58 -0500

    style(Product):agregue un archivo para stylos y agregue Nameclass y una funcion para la descripcion

--- marly-handmade-frontend/src/pages/Product.jsx
+++ marly-handmade-frontend/src/pages/Product.jsx
@@ -1,3 +1,4 @@
+
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import MostLoved from "../components/MostLoved";
@@ -51,7 +52,7 @@ export default function Product() {
                         <button className="button-add-to-cart">ADD TO CART</button> 
                         <button className="button-buy-now">BUY NOW</button> 
                         <DescriptionItem title="Description">
-                        <p id="description"></p>
+                        <p id="description">cualquier cosa</p>
                         </DescriptionItem>
                         
                         <DescriptionItem title="Product Details">
@@ -75,86 +76,3 @@ export default function Product() {
         </>
     );
 }
-
-/*
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import MostLoved from "../components/MostLoved";
-import ProductSelection from "../assets/image42.png";
-import SelectAmount from "../components/Amount";
-import ProductCard from "../components/ProductCard";
-import ProductGrid from "../components/ProductGrid";
-import "../components/Product.css";
-import "../components/ProductSelect.css";
-
-import { useState } from "react";
-
-//con este funcion ayuda aabrir y cerra la description osea del + y menos
-function DescriptionItem({ title, children }) {
-  const [open, setOpen] = useState(false);
-
-  return (
-    <div className="accordion-item">
-      <p
-        className="product-detail-accordion"
-        onClick={() => setOpen(!open)}
-      >
-        {title}
-        <span><strong>{open ? "-" : "+"}</strong></span>
-      </p>
-      {open && <div className="accordion-content">{children}</div>}
-    </div>
-  );
-}
-
-export default function Product() {
-    return (
-        <>
-            <Header />
-
-            <section className="content-Product-Select">
-                <div className="Img-Product">
-                    <img src={ProductSelection} />
-                </div>
-                <div className="Content product-details-content"> 
-                    <p className="Sub-Name-Product">Home / <p id="NameProduct"></p></p>
-                    <br/>
-                    <h2 className="Name-Producto" id="NameProduct"></h2>
-
-                    <p className="product-price" id="price"></p> 
-                    <h2 className="product-variant-name" id="Category"></h2> 
-                    
-                    <div className="amaunt-select">
-                        <SelectAmount />
-                    </div>
-
-                    <div className="content">
-                        <button className="button-add-to-cart">ADD TO CART</button> 
-                        <button className="button-buy-now">BUY NOW</button> 
-                        <DescriptionItem title="Description">
-                        <p id="description"></p>
-                        </DescriptionItem>
-                        
-                        <DescriptionItem title="Product Details">
-                        ponga cualquier cosa
-                        </DescriptionItem>
-
-                        <DescriptionItem title="Jewelry Care">
-                        cualquier cosa lo pongo
-                        </DescriptionItem>
-
-                        <DescriptionItem title="Shipping Info">
-                        ponga cualquier cosa
-                        </DescriptionItem>
-                    </div>
-                </div>
-            </section>
-      
-            <MostLoved />
-
-            <Footer />
-        </>
-    );
-}
-
-*/
