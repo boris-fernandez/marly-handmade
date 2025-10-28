1a90a99fecb05998df205e1f36cf1a0208b16177
Josue049 <josue09guti@gmail.com>
2025-10-28 01:15:12 -0500

    feat(UsersAdmin): conection con backend

--- marly-handmade-frontend/src/main.jsx
+++ marly-handmade-frontend/src/main.jsx
@@ -8,18 +8,21 @@ import { AuthProviderWrapper } from "./contexts/AuthContext.jsx";
 import { CartProvider } from "./contexts/CartContext.jsx";
 import { ReclamacionesProvider } from "./contexts/ReclamacionesContext";
 import { ProductoProvider } from "./contexts/ProductoContext";
+import { AdminProvider } from "./contexts/AdminContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProviderWrapper>
       <ProductoProvider>
-        <ReclamacionesProvider>
-          <CartProvider>
-            <BrowserRouter>
-              <App />
-            </BrowserRouter>
-          </CartProvider>
-        </ReclamacionesProvider>
+        <AdminProvider>
+          <ReclamacionesProvider>
+            <CartProvider>
+              <BrowserRouter>
+                <App />
+              </BrowserRouter>
+            </CartProvider>
+          </ReclamacionesProvider>
+        </AdminProvider>
       </ProductoProvider>
     </AuthProviderWrapper>
   </StrictMode>
