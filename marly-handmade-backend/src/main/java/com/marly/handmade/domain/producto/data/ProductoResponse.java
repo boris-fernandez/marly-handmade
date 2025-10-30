40758a34eff43d9e58044df563657ddd2e284e61
Josué Ángel Gutiérrez Cuéllar <77085961+Josue049@users.noreply.github.com>
2025-10-30 03:26:33 -0500

    Refactor ProductoResponse constructor for clarity

--- marly-handmade-backend/src/main/java/com/marly/handmade/domain/producto/data/ProductoResponse.java
+++ marly-handmade-backend/src/main/java/com/marly/handmade/domain/producto/data/ProductoResponse.java
@@ -16,20 +16,20 @@ public record ProductoResponse(
         String shipping_info) {
 
     public ProductoResponse(Producto producto) {
-        this(
-                producto.getIdProducto(),
-                producto.getNombre(),
-                producto.getDescripcion(),
-                producto.getPrecio(),
-                producto.getStock(),
-                producto.getFotoPrincipal(),
-                producto.getFotoSecundario(),
-                producto.getFotoTerciario(),
-                producto.getCategoria(),
-                producto.getDetails(),
-                producto.getCare(),
-                producto.getShippingInfo()
-        );
-    }
+    this(
+        producto.getIdProducto(),     // Long id
+        producto.getNombre(),         // String nombre
+        producto.getPrecio(),         // Double precio
+        producto.getStock(),          // Integer stock
+        producto.getFotoPrincipal(),  // String fotoPrincipal
+        producto.getFotoSecundario(), // String fotoSecundario
+        producto.getFotoTerciario(),  // String fotoTerciario
+        producto.getCategoria(),      // String categoria
+        producto.getDetails(),        // String details
+        producto.getCare(),           // String care
+        producto.getShippingInfo()    // String shipping_info
+    );
+}
+
 
 }
